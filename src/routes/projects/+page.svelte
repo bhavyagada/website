e26b97042ccc1cde2d74acabc8a76924d35f4cf8
bhavya <bsg@somaiya.edu>
2024-09-04 17:22:05 -0700

    Add the watdat project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,22 @@
-<main>
-  Projects
+<script>
+  import watdat from "$lib/images/watdat.png";
+</script>
+
+<svelte:head>
+  <title>Projects | Bhavya Gada</title>
+</svelte:head>
+
+<main class="font-serif">
+  <section>
+    <h2 class="text-xl">Wat dat</h2>
+    <a href="https://addons.mozilla.org/addon/watdat/" class="text-blue-700 hover:underline">live</a> |
+    <a href="https://github.com/bhavyagada/watdat" class="text-blue-700 hover:underline">code</a>
+    <p class="py-2">
+      Wat dat (What's that?) is a Firefox/Chrome extension that sends your highlighted text to an LLM and provides its explanation.
+    </p>
+    <p class="py-2">
+      This eliminates the need to copy-pasting text into an LLM, helps maintain your workflow, and reduces context-switching.
+    </p>
+    <img src={watdat} alt="wat dat screenshot" class="py-2" />
+  </section>
 </main>
