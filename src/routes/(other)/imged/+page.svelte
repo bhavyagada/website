1434d9813c2114532f5fb9d85f0940473c4b91a1
bhavya <bsg@somaiya.edu>
2024-09-23 23:18:11 -0700

    change toolbar buttons order

--- src/routes/(other)/imged/+page.svelte
+++ src/routes/(other)/imged/+page.svelte
@@ -24,14 +24,14 @@
 <div class="relative">
   <canvas id="c" class="w-screen h-screen block"></canvas>
   <div class="absolute bg-white text-sm text-black font-mono p-2 left-2.5 top-2.5 leading-4">
+    <div class="flex items-center gap-2"><img src={dptIcon} alt="DPT Icon" width="15" height="15"> depthwise BG removal</div>
+    <div class="flex items-center gap-2"><img src={segmentIcon} alt="Segment Icon" width="15" height="15"> segment</div>
     <div class="flex items-center gap-2"><img src={backIcon} alt="Back Icon" width="15" height="15"> move to back</div>
     <div class="flex items-center gap-2"><img src={flipIcon} alt="Flip Icon" width="15" height="15"> flip</div>
     <div class="flex items-center gap-2"><img src={duplicateIcon} alt="Duplicate Icon" width="15" height="15"> duplicate</div>
-    <div class="flex items-center gap-2"><img src={segmentIcon} alt="Segment Icon" width="15" height="15"> segment</div>
+    <div class="flex items-center gap-2"><img src={cropIcon} alt="Crop Icon" width="15" height="15"> crop</div>
     <div class="flex items-center gap-2"><img src={downloadIcon} alt="Download Icon" width="15" height="15"> download</div>
     <div class="flex items-center gap-2"><img src={deleteIcon} alt="Delete Icon" width="15" height="15"> delete</div>
-    <div class="flex items-center gap-2"><img src={cropIcon} alt="Crop Icon" width="15" height="15"> crop</div>
-    <div class="flex items-center gap-2"><img src={dptIcon} alt="DPT Icon" width="15" height="15"> depthwise BG removal</div>
   </div>
 </div>
 
