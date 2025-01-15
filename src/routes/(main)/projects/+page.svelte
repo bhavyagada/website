00c803ff2a00097b9eb6779ec6a949d02dfc18ab
bhavya <bsg@somaiya.edu>
2025-01-15 01:38:15 -0800

    update packages

--- src/routes/(main)/projects/+page.svelte
+++ src/routes/(main)/projects/+page.svelte
@@ -1,8 +1,8 @@
 <script>
   import watdat_v from "$lib/videos/watdat.mp4";
   import watdat_c from "$lib/videos/watdat.vtt";
-  import watdat_hn from "$lib/images/watdat_hn.png";
-  import watdat_shn from "$lib/images/watdat_shn.png";
+  // import watdat_hn from "$lib/images/watdat_hn.png";
+  // import watdat_shn from "$lib/images/watdat_shn.png";
 </script>
 
 <svelte:head>
@@ -17,24 +17,12 @@
     </div>
     <a href="/imged" target="_blank" class="text-blue-700 hover:underline">live</a> |
     <a href="https://github.com/bhavyagada/webgl" target="_blank" class="text-blue-700 hover:underline">code</a>
-    <p class="py-2 text-xl">
-      [work in progress] so, expect a lot of bugs!
-    </p>
     <p class="py-2">
-      I'm trying to learn WebGL by building an Image Editor (Imged) similar to
+      [wip] Learning WebGL by building an Image Editor (Imged) similar to
       <a href="https://dingboard.com" target="_blank" class="text-blue-700 hover:underline">dingboard</a>.
     </p>
-    <p class="py-2">
-      Everything rendered on the canvas is done purely using WebGL2.
-    </p>
-    <p class="py-2">
-      So far, users can paste multiple images, select and drag them around, delete selected images, segment images, crop them, perform depthwise background removal, and undo their actions (incomplete).
-    </p>
-    <p class="py-2">
-      It ain't much, but it's honest work.
-    </p>
   </section>
-  
+  <hr class="text-black">
   <section>
     <div class="flex w-full py-4">
       <u class="flex-1 text-xl">Wat dat</u>
@@ -49,17 +37,16 @@
     <p class="py-2">
       This eliminates the need to copy-pasting text into an LLM, helps maintain your workflow, and reduces context-switching.
     </p>
-    <video controls>
-      <source src={watdat_v} type="video/mp4" />
-      <track default kind="captions" src={watdat_c}>
-    </video>
     <p class="py-2">
       This project briefly reached the #13 spot on the front page of 
       <a href="https://news.ycombinator.com/item?id=41453283" target="_blank" class="text-blue-700 hover:underline">Hacker News</a>.
     </p>
-    <img src={watdat_hn} alt="wat dat #13 on hacker news" class="py-2" />
-    <p class="py-2">And, briefly reached the #3 spot on the Show HN page of Hacker News.</p>
-    <img src={watdat_shn} alt="wat dat #3 on show hacker news" class="py-2" />
+    <video controls>
+      <source src={watdat_v} type="video/mp4" />
+      <track default kind="captions" src={watdat_c}>
+    </video>
+    <!-- <img src={watdat_hn} alt="wat dat #13 on hacker news" class="py-2" /> -->
+    <!-- <img src={watdat_shn} alt="wat dat #3 on show hacker news" class="py-2" /> -->
   </section>
 </main>
 
