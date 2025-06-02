ca85ada9be1729cc271905163d49ab675bbcb75f
bhavya <bsg@somaiya.edu>
2025-06-02 16:15:57 -0700

    remove blog for now

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -12,8 +12,6 @@
     <p>Software Engineer</p>
     <h2>What I'm doing now</h2>
     <p>Job hunting, leetcode and stuff!</p>
-    <h2>Weblog</h2>
-    <p>See <a href="/blog">my "blog" page</a>.</p>
     <h2>Projects</h2>
     <p>See <a href="/projects">my "projects" page</a>.</p>
     <h2>Social</h2>
